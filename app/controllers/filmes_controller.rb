2ad3fa70be2687d3f645f32e2347b2145851010d
Tâmara Barbosa <tamara-bt@hotmail.com>
2018-07-19 02:08:12 -0300

    Corrigindo Bugs

--- app/controllers/filmes_controller.rb
+++ app/controllers/filmes_controller.rb
@@ -69,6 +69,6 @@ class FilmesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def filme_params
-      params.require(:filme).permit(:nome, :genero, :preco_de_locacao)
+      params.require(:filme).permit(:nome, :genero, :genero_id, :preco_de_locacao)
     end
 end
