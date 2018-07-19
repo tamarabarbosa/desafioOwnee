d8c1cb23bbc33c1b337a58875f4209e1f2746534
Tâmara Barbosa <tamara-bt@hotmail.com>
2018-07-19 01:38:55 -0300

    Criando seeds para popular base

---
+++ db/seeds.rb
@@ -0,0 +1,35 @@
+Genero.create([
+  {id => 1, :nome => 'Ação', :created_at => "2018-07-19", :updated_at => "2018-07-19"},
+  {id => 2, :nome => 'Animação', :created_at => "2018-07-19", :updated_at => "2018-07-19"},
+  {id => 3, :nome => 'Aventura', :created_at => "2018-07-19", :updated_at => "2018-07-19"},
+  {id => 4, :nome => 'Comédia', :created_at => "2018-07-19", :updated_at => "2018-07-19"},
+  {id => 5, :nome => 'Documentário', :created_at => "2018-07-19", :updated_at => "2018-07-19"},
+  {id => 6, :nome => 'Fantasia', :created_at => "2018-07-19", :updated_at => "2018-07-19"},
+  {id => 7, :nome => 'Faroeste', :created_at => "2018-07-19", :updated_at => "2018-07-19"},
+  {id => 8, :nome => 'Ficção científica', :created_at => "2018-07-19", :updated_at => "2018-07-19"},
+  {id => 9, :nome => 'Guerra', :created_at => "2018-07-19", :updated_at => "2018-07-19"},
+  {id => 10, :nome => 'Musical', :created_at => "2018-07-19", :updated_at => "2018-07-19"},
+  {id => 11, :nome => 'Pornográfico', :created_at => "2018-07-19", :updated_at => "2018-07-19"},
+  {id => 12, :nome => 'Romance', :created_at => "2018-07-19", :updated_at => "2018-07-19"},
+  {id => 13, :nome => 'Suspense', :created_at => "2018-07-19", :updated_at => "2018-07-19"},
+  {id => 14, :nome => 'Terror', :created_at => "2018-07-19", :updated_at => "2018-07-19"},
+  {id => 15, :nome => 'Drama', :created_at => "2018-07-19", :updated_at => "2018-07-19"}
+  ])
+
+Filme.create([
+  {:id => 1, :nome => 'Vingadores: Guerra Infinita', :genero_id => 1, :preco_de_locacao => 10.00, :created_at => "2018-07-19", :updated_at => "2018-07-19"},
+  {:id => 2, :nome => 'Os Incríveis 2', :genero_id => 2, :preco_de_locacao => 10.00, :created_at => "2018-07-19", :updated_at => "2018-07-19"},
+  {:id => 3, :nome => 'Homem-Formiga e a Vespa', :genero_id => 3, :preco_de_locacao => 10.00, :created_at => "2018-07-19", :updated_at => "2018-07-19"},
+  {:id => 4, :nome => 'Deadpool 2', :genero_id => 4, :preco_de_locacao => 10.00, :created_at => "2018-07-19", :updated_at => "2018-07-19"},
+  {:id => 5, :nome => 'O Processo', :genero_id => 5, :preco_de_locacao => 10.00, :created_at => "2018-07-19", :updated_at => "2018-07-19"},
+  {:id => 6, :nome => 'Pantera Negra', :genero_id => 6, :preco_de_locacao => 10.00, :created_at => "2018-07-19", :updated_at => "2018-07-19"},
+  {:id => 7, :nome => 'Os Oito Odiados', :genero_id => 7, :preco_de_locacao => 10.00, :created_at => "2018-07-19", :updated_at => "2018-07-19"},
+  {:id => 8, :nome => 'Blade Runner', :genero_id => 8, :preco_de_locacao => 10.00, :created_at => "2018-07-19", :updated_at => "2018-07-19"},
+  {:id => 9, :nome => 'Capitão América: Gerra Civil', :genero_id => 9, :preco_de_locacao => 10.00, :created_at => "2018-07-19", :updated_at => "2018-07-19"},
+  {:id => 10, :nome => 'Frozen - Uma Aventura Congelante', :genero_id => 10, :preco_de_locacao => 10.00, :created_at => "2018-07-19", :updated_at => "2018-07-19"},
+  {:id => 11, :nome => 'Brasileirinhas', :genero_id => 11, :preco_de_locacao => 10.00, :created_at => "2018-07-19", :updated_at => "2018-07-19"},
+  {:id => 12, :nome => 'Como eu era Antes de Você', :genero_id => 12, :preco_de_locacao => 10.00, :created_at => "2018-07-19", :updated_at => "2018-07-19"},
+  {:id => 13, :nome => 'Corra!', :genero_id => 13, :preco_de_locacao => 10.00, :created_at => "2018-07-19", :updated_at => "2018-07-19"},
+  {:id => 14, :nome => 'It: A Coisa', :genero_id => 14, :preco_de_locacao => 10.00, :created_at => "2018-07-19", :updated_at => "2018-07-19"},
+  {:id => 15, :nome => 'O Extraordinário', :genero_id => 15, :preco_de_locacao => 10.00, :created_at => "2018-07-19", :updated_at => "2018-07-19"}
+  ])
